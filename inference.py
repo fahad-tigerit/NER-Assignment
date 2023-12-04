9e4cda259bd81eff2bf77d8c6c78562b3b65e7fe
fahad-tigerit <fahad.rahman@google.com>
2023-12-04 12:22:42 +0600

    new model added

--- inference.py
+++ inference.py
@@ -17,21 +17,39 @@ import datasets
 import numpy as np
 from transformers import pipeline
 
-tokenizer = BertTokenizerFast.from_pretrained("tokenizer")
+tokenizer = BertTokenizerFast.from_pretrained("tokenizer_only_train")
 
-model_fine_tuned = AutoModelForTokenClassification.from_pretrained("ner_model")
+model_fine_tuned = AutoModelForTokenClassification.from_pretrained(
+    "ner_model_only_train"
+)
 
 nlp = pipeline("ner", model=model_fine_tuned, tokenizer=tokenizer)
+
+# %%
+dataset = load_dataset("tuetschek/atis")
+test_dataset = dataset["train"]
+test_texts = test_dataset["text"]
+print(type(test_dataset))
+
+
+# %%
+print(len(test_texts))
+for lines in test_texts:
+    example = lines
+    ner_results = nlp(example)
+    for x in ner_results:
+        # print(x["entity"], x["word"])
+        if "B-return" in x["entity"] or "I-return" in x["entity"]:
+            print(x["entity"], x["word"])
 
 # %%
 # import transformers
 # print(transformers.__version__)
 
-example = "how about a flight from milwaukee to st. louis that leaves monday night"
+example = "i need a return flight from philadelphia to boston"
 ner_results = nlp(example)
 for x in ner_results:
+    # print(x["entity"], x["word"])
+
     print(x["entity"], x["word"])
     # print(type(x))
-
-
-# %%
--- training.py
+++ training.py
@@ -142,9 +142,6 @@ trainer = Trainer(
 trainer.train()
 model.save_pretrained("ner_model")
 tokenizer.save_pretrained("tokenizer")
-
-# %%
-
 id2label = {str(i): label for i, label in enumerate(lookup_table)}
 label2id = {label: str(i) for i, label in enumerate(lookup_table)}
 
@@ -156,6 +153,8 @@ config = json.load(open("ner_model/config.json"))
 config["id2label"] = id2label
 config["label2id"] = label2id
 json.dump(config, open("ner_model/config.json", "w"))
+
+# %%
 
 
 model_fine_tuned = AutoModelForTokenClassification.from_pretrained("ner_model")
